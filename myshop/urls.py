1c666eb0c116eee56ed4b73b56a785e6a478fa8e
jericho <elric2009@gmail.com>
2020-08-01 16:22:47 +1000

    add create and created templates into order app

--- myshop/urls.py
+++ myshop/urls.py
@@ -19,9 +19,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('shop.urls', namespace='shop')),
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls', namespace='cart')),
-    path('', include('shop.urls', namespace='shop')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]
 
 if settings.DEBUG:
