c2dad44d423d2cebcdc08d76a59191aaddf52276
holic <elric2009@gmail.com>
2020-08-07 15:35:35 +1000

    fix bugs into admin and views from orders app

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
-from admin.contrib.admin.views.decorators import staff_member_required
+from django.contrib.admin.views.decorators import staff_member_required
 
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 
@@ -42,7 +42,7 @@ def order_create(request):
 
 @staff_member_required
 def admin_order_detail(request, order_id):
-    order = get_object_or_404(request, order_id)
+    order = get_object_or_404(Order, id=order_id)
     return render(
         request,
         'admin/orders/order/detail.html',
