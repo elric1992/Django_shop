717fdf6170cca685e875d103ef47753732f065f1
holic <elric2009@gmail.com>
2020-08-07 11:16:35 +1000

    add def order_detail into admin.py in order app

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,8 @@ import datetime
 
 from django.contrib import admin
 from django.http import HttpResponse
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Order, OrderItem
 
@@ -13,7 +15,6 @@ def export_to_csv(modeladmin, request, queryset):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = content_disposition
     writer = csv.writer(response)
-
     fields = [
         field for field in opts.get_fields() \
             if not field.many_to_many and not field.one_to_many
@@ -28,7 +29,6 @@ def export_to_csv(modeladmin, request, queryset):
             if isinstance(value, datetime.datetime):
                 value = value.strftime('%d/%m/%Y')
             data_row.append(value)
-
         writer.writerow(data_row)
     return response
     
@@ -38,6 +38,11 @@ export_to_csv.short_description = 'Export to CSV'
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
 
 
 @admin.register(Order)
