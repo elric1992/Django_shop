7ade3e39b765c5605469e51e8ea8d5ccdaf040ae
holic <elric2009@gmail.com>
2020-09-08 17:32:26 +1000

    page 353

--- orders/models.py
+++ orders/models.py
@@ -1,29 +1,31 @@
 from django.db import models
 from decimal import Decimal
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.translation import gettext_lazy as _
 
 from shop.models import Product
 from coupons.models import Coupon
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    paid = models.BooleanField(default=False)
-    braintree_id = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(_('first_name'), max_length=50)
+    last_name = models.CharField(_('last_name'), max_length=50)
+    email = models.EmailField(_('e-mail'))
+    address = models.CharField(_('address'), max_length=250)
+    postal_code = models.CharField(_('postal code'), max_length=20)
+    city = models.CharField(_('city'), max_length=100)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    updated = models.DateTimeField(_('updated'), auto_now=True)
+    paid = models.BooleanField(_('paid'), default=False)
+    braintree_id = models.CharField(_('braintree id'), max_length=150, blank=True)
     coupon = models.ForeignKey(
+        _('coupon'),
         Coupon,
         related_name='orders',
         null=True,
         blank=True,
         on_delete=models.SET_NULL
-        )
+    )
     discount = models.IntegerField(
         default=0,
         validators=[MinValueValidator(0), MaxValueValidator(100)]
